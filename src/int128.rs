0c6bcfe7b6228d6637c78dad7571778bc4c08c76
wub <wubingzheng@gmail.com>
2026-03-28 22:18:33 +0800

    change layout!

--- src/int128.rs
+++ src/int128.rs
@@ -1,111 +1,67 @@
 use crate::UnderlyingInt;
-use div_pow10::bit128;
 
-const ALL_EXPS: [i128; 39] = [
+const ALL_EXPS: [u128; 39] = [
     1,
-    10_i128.pow(1),
-    10_i128.pow(2),
-    10_i128.pow(3),
-    10_i128.pow(4),
-    10_i128.pow(5),
-    10_i128.pow(6),
-    10_i128.pow(7),
-    10_i128.pow(8),
-    10_i128.pow(9),
-    10_i128.pow(10),
-    10_i128.pow(11),
-    10_i128.pow(12),
-    10_i128.pow(13),
-    10_i128.pow(14),
-    10_i128.pow(15),
-    10_i128.pow(16),
-    10_i128.pow(17),
-    10_i128.pow(18),
-    10_i128.pow(19),
-    10_i128.pow(20),
-    10_i128.pow(21),
-    10_i128.pow(22),
-    10_i128.pow(23),
-    10_i128.pow(24),
-    10_i128.pow(25),
-    10_i128.pow(26),
-    10_i128.pow(27),
-    10_i128.pow(28),
-    10_i128.pow(29),
-    10_i128.pow(30),
-    10_i128.pow(31),
-    10_i128.pow(32),
-    10_i128.pow(33),
-    10_i128.pow(34),
-    10_i128.pow(35),
-    10_i128.pow(36),
-    10_i128.pow(37),
-    10_i128.pow(38),
+    10_u128.pow(1),
+    10_u128.pow(2),
+    10_u128.pow(3),
+    10_u128.pow(4),
+    10_u128.pow(5),
+    10_u128.pow(6),
+    10_u128.pow(7),
+    10_u128.pow(8),
+    10_u128.pow(9),
+    10_u128.pow(10),
+    10_u128.pow(11),
+    10_u128.pow(12),
+    10_u128.pow(13),
+    10_u128.pow(14),
+    10_u128.pow(15),
+    10_u128.pow(16),
+    10_u128.pow(17),
+    10_u128.pow(18),
+    10_u128.pow(19),
+    10_u128.pow(20),
+    10_u128.pow(21),
+    10_u128.pow(22),
+    10_u128.pow(23),
+    10_u128.pow(24),
+    10_u128.pow(25),
+    10_u128.pow(26),
+    10_u128.pow(27),
+    10_u128.pow(28),
+    10_u128.pow(29),
+    10_u128.pow(30),
+    10_u128.pow(31),
+    10_u128.pow(32),
+    10_u128.pow(33),
+    10_u128.pow(34),
+    10_u128.pow(35),
+    10_u128.pow(36),
+    10_u128.pow(37),
+    10_u128.pow(38),
 ];
 
-// maxmax = pow(2, 121) - 1
-// for i in range(1,40):
-//     print("%d," % (maxmax // pow(10, i)))
-const AVAIL_THRESHOLD: [i128; 37] = [
-    265845599156983174580761412056068915,
-    26584559915698317458076141205606891,
-    2658455991569831745807614120560689,
-    265845599156983174580761412056068,
-    26584559915698317458076141205606,
-    2658455991569831745807614120560,
-    265845599156983174580761412056,
-    26584559915698317458076141205,
-    2658455991569831745807614120,
-    265845599156983174580761412,
-    26584559915698317458076141,
-    2658455991569831745807614,
-    265845599156983174580761,
-    26584559915698317458076,
-    2658455991569831745807,
-    265845599156983174580,
-    26584559915698317458,
-    2658455991569831745,
-    265845599156983174,
-    26584559915698317,
-    2658455991569831,
-    265845599156983,
-    26584559915698,
-    2658455991569,
-    265845599156,
-    26584559915,
-    2658455991,
-    265845599,
-    26584559,
-    2658455,
-    265845,
-    26584,
-    2658,
-    265,
-    26,
-    2,
-    0,
-];
-
-fn get_exp(i: u8) -> i128 {
+fn get_exp(i: u32) -> u128 {
     debug_assert!(i <= 36);
     unsafe { *ALL_EXPS.get_unchecked(i as usize) }
 }
 
-impl UnderlyingInt for i128 {
+impl UnderlyingInt for u128 {
+    const ZERO: Self = 0;
+    const ONE: Self = 1;
+    const TEN: Self = 10;
     const MAX: Self = Self::MAX;
     const MIN: Self = Self::MIN;
-    const ZERO: Self = 0;
-    const TEN: Self = 10;
-    const BITS: u8 = 128;
-    const MATISSA_DIGITS: u8 = 36;
-    const SCALE_BITS: u8 = 6;
-    const MAX_SCALE: u8 = 36;
-    const IS_SIGNED: bool = true;
 
-    fn as_u8(self) -> u8 {
-        self as u8
+    const BITS: u32 = 128;
+    const MAX_SCALE: u32 = 36;
+    const MAX_MATISSA: Self = Self::MAX >> (Self::SCALE_BITS + 1);
+
+    fn as_u32(self) -> u32 {
+        self as u32
     }
-    fn from_u8(n: u8) -> Self {
+    fn from_u32(n: u32) -> Self {
         n as Self
     }
 
@@ -113,66 +69,37 @@ impl UnderlyingInt for i128 {
         self.leading_zeros()
     }
 
-    fn avail_digits(self) -> u8 {
-        let n = self.abs() | 1; // make 0 -> 1
-        let bits = n.leading_zeros() - Self::SCALE_BITS as u32 - 1; // 1 for sign
-        let digits = bits * 1233 >> 12; // math!
-        let ath = unsafe { AVAIL_THRESHOLD.get_unchecked(digits as usize) };
-        digits as u8 + (&n <= ath) as u8
-    }
+    fn mul_with_sum_scale(self, right: Self, sum_scale: u32) -> Option<(Self, u32)> {
+        let (high, low) = mul2(self, right);
 
-    fn is_mul_overflow(self, right: Self) -> bool {
-        let ua = self.unsigned_abs();
-        let ub = right.unsigned_abs();
-        ((256 - ua.leading_zeros() - ub.leading_zeros()) as u8) >= 128 - Self::SCALE_BITS - 1
-    }
+        // Calculate the digits that needed be cleared.
+        // We do not calculate it accurately for performance.
+        // The clear_digits may be bigger than needed.
+        let clear_bits = 128 + Self::SCALE_BITS + 1 - high.leading_zeros(); // ignore @low
+        let mut clear_digits = (clear_bits * 1233 >> 12) + 1; // +1 for ceiling
 
-    #[cold]
-    fn mul_with_sum_scale(self, right: Self, sum_scale: u8) -> Option<(Self, u8)> {
-        let ua = self.unsigned_abs();
-        let ub = right.unsigned_abs();
-        // if ((256 - ua.leading_zeros() - ub.leading_zeros()) as u8) < 128 - Self::SCALE_BITS - 1 {
-        //     let p = self * right;
-        //     if sum_scale <= Self::MAX_SCALE {
-        //         Some((p, sum_scale))
-        //     } else {
-        //         let diff = sum_scale - Self::MAX_SCALE;
-        //         Some((p.div_exp(diff), Self::MAX_SCALE))
-        //     }
-        // } else {
-        let (high, low) = mul2(ua, ub);
-        let extra = (high << (Self::SCALE_BITS + 1)) | (low >> (128 - Self::SCALE_BITS - 1));
-        let digits = (128 - extra.leading_zeros()) * 1233 >> 12;
-        let mut more_digits = digits as u8 + ((extra > get_exp(digits as u8) as u128) as u8);
-
-        if sum_scale <= Self::MAX_SCALE {
-            if more_digits > sum_scale {
-                return None;
-            }
-        } else {
-            more_digits = more_digits.max(sum_scale - Self::MAX_SCALE);
+        // Reduce sum_scale if too big.
+        if sum_scale > Self::MAX_SCALE {
+            clear_digits = clear_digits.max(sum_scale - Self::MAX_SCALE);
         }
 
-        let (q, _) = unsafe { bit128::unchecked_div_double(high, low, more_digits as u32) };
+        // overflow
+        if clear_digits > sum_scale {
+            return None;
+        }
 
-        let q = q as i128;
-
-        let q = if self ^ right < 0 { -q } else { q };
-        Some((q, sum_scale - more_digits))
-        // }
+        let (q, _) = unsafe { div_pow10::bit128::unchecked_div_double(high, low, clear_digits) };
+        Some((q, sum_scale - clear_digits))
     }
 
     // caller must make sure that no overflow
-    fn mul_exp(self, iexp: u8) -> Self {
+    fn mul_exp(self, iexp: u32) -> Self {
         self * get_exp(iexp)
     }
 
-    #[inline] // TODO good for mac, bad for linux
-    fn div_exp(self, iexp: u8) -> Self {
-        let exp = get_exp(iexp);
-        let n = self.unsigned_abs() + exp as u128 / 2; // no addition overflow. exp is even.
-        let q = unsafe { bit128::unchecked_div_single_r1b(n, iexp as u32) } as i128;
-        if self >= 0 { q } else { -q }
+    fn div_exp(self, iexp: u32) -> Self {
+        let n = self + get_exp(iexp) / 2; // no addition overflow. exp is even.
+        unsafe { div_pow10::bit128::unchecked_div_single_r1b(n, iexp) }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod int128;
 
-use core::ops::{Add, AddAssign, BitOr, Div, Mul, Shl, Shr, Sub, SubAssign};
+use core::ops::{Add, AddAssign, BitAnd, BitOr, Div, Mul, Shl, Shr, Sub, SubAssign};
 
 #[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq, Hash, Default)]
 #[repr(transparent)]
@@ -20,162 +20,167 @@ pub trait UnderlyingInt:
     + Sub<Output = Self>
     + Div<Output = Self>
     + Mul<Output = Self>
-    + Shl<u8, Output = Self>
-    + Shr<u8, Output = Self>
+    + Shl<u32, Output = Self>
+    + Shr<u32, Output = Self>
     + BitOr<Output = Self>
+    + BitAnd<Output = Self>
     + AddAssign
     + SubAssign
 {
+    const ZERO: Self;
+    const ONE: Self;
+    const TEN: Self;
     const MAX: Self;
     const MIN: Self;
-    const ZERO: Self;
-    const TEN: Self;
-    const BITS: u8;
-    const MATISSA_DIGITS: u8;
-    const SCALE_BITS: u8;
-    const MAX_SCALE: u8;
-    const IS_SIGNED: bool;
 
-    fn as_u8(self) -> u8;
-    fn from_u8(n: u8) -> Self;
+    const BITS: u32;
+    const SCALE_BITS: u32 = (Self::MAX_SCALE * 2 - 1).ilog2();
+    const MAX_SCALE: u32;
+    const MAX_MATISSA: Self;
+
+    fn as_u32(self) -> u32;
+    fn from_u32(n: u32) -> Self;
 
     fn leading_zeros(self) -> u32;
 
-    fn avail_digits(self) -> u8;
-
     // caller has made sure that no overflow
-    fn mul_exp(self, iexp: u8) -> Self;
+    fn mul_exp(self, iexp: u32) -> Self;
 
-    fn div_exp(self, iexp: u8) -> Self;
+    fn div_exp(self, iexp: u32) -> Self;
 
-    fn mul_with_sum_scale(self, right: Self, sum_scale: u8) -> Option<(Self, u8)>;
+    fn mul_with_sum_scale(self, right: Self, sum_scale: u32) -> Option<(Self, u32)>;
 
-    fn is_mul_overflow(self, right: Self) -> bool;
+    fn min_avail_digits(self) -> u32 {
+        let bits = self.leading_zeros() - Self::SCALE_BITS - 1;
+        bits * 1233 >> 12 // math!
+    }
 }
 
 impl<I: UnderlyingInt> Decimal<I> {
     pub const ZERO: Self = Self(I::ZERO);
 
-    pub fn mantissa(self) -> I {
-        self.0 >> I::SCALE_BITS
+    // layout:
+    //   +-+-----+-------------+
+    //   |S|scale|  mantissa   |
+    //   +-+-----+-------------+
+    pub fn unpack(self) -> (u8, u32, I) {
+        let mantissa = self.0 & I::MAX_MATISSA;
+        let meta = (self.0 >> (I::BITS - 1 - I::SCALE_BITS)).as_u32();
+        let scale = meta & ((1 << I::SCALE_BITS) - 1);
+        let sign = meta >> I::SCALE_BITS;
+        (sign as u8, scale, mantissa)
     }
 
-    pub fn scale(self) -> u8 {
-        self.0.as_u8() & ((1 << I::SCALE_BITS) - 1)
-    }
-
-    pub fn unpack(self) -> (I, u8) {
-        (self.mantissa(), self.scale())
-    }
-
-    pub fn pack(mantissa: I, scale: u8) -> Option<Self> {
-        if !Self::valid_matissa(mantissa) {
+    pub fn pack(sign: u8, scale: u32, mantissa: I) -> Option<Self> {
+        if sign > 1 {
             return None;
         }
         if scale > I::MAX_SCALE {
             return None;
         }
-        Some(Self::do_pack(mantissa, scale))
+        if mantissa > I::MAX_MATISSA {
+            return None;
+        }
+        Some(Self::do_pack(sign, scale, mantissa))
     }
 
-    // the caller must make sure that @mantissa and @scale are valid
-    fn do_pack(mantissa: I, scale: u8) -> Self {
-        debug_assert!(Self::valid_matissa(mantissa));
+    // the caller must make sure that the inputs are valid
+    fn do_pack(sign: u8, scale: u32, mantissa: I) -> Self {
+        debug_assert!(sign <= 1);
         debug_assert!(scale <= I::MAX_SCALE);
-        Self((mantissa << I::SCALE_BITS) | I::from_u8(scale))
+        debug_assert!(mantissa <= I::MAX_MATISSA);
+
+        let meta = ((sign as u32) << I::SCALE_BITS) | scale;
+        Self(I::from_u32(meta) << (I::BITS - 1 - I::SCALE_BITS) | mantissa)
     }
 
-    fn valid_matissa(m: I) -> bool {
-        m <= I::MAX >> I::SCALE_BITS && m >= I::MIN >> I::SCALE_BITS
+    pub fn checked_add(self, right: Self) -> Option<Self> {
+        self.add_or_sub(right, true)
     }
 
-    // the caller must make sure that @ma and @mb are valid
-    fn pack_sum(sum: I, scale: u8) -> Option<Self> {
-        if Self::valid_matissa(sum) {
-            Some(Self::do_pack(sum, scale))
+    pub fn checked_sub(self, right: Self) -> Option<Self> {
+        self.add_or_sub(right, false)
+    }
+
+    fn add_or_sub(self, right: Self, is_add: bool) -> Option<Self> {
+        let (a_sign, a_scale, a_man) = self.unpack();
+        let (b_sign, b_scale, b_man) = right.unpack();
+
+        let (a_man, b_man, scale) = if a_scale == b_scale {
+            (a_man, b_man, a_scale)
+        } else {
+            align_scale(a_man, a_scale, b_man, b_scale)
+        };
+
+        // do the addition or substraction
+        let (sign, sum) = if (a_sign == b_sign) == is_add {
+            (a_sign, a_man + b_man)
+        } else if a_man > b_man {
+            (a_sign, a_man - b_man)
+        } else {
+            (b_sign, b_man - a_man)
+        };
+
+        // pack
+        if sum <= I::MAX_MATISSA {
+            Some(Self::do_pack(sign, scale, sum))
         } else if scale > 0 {
-            Some(Self::do_pack(sum / I::TEN, scale - 1)) // TODO round div
+            let man = (sum + (I::TEN >> 1)) / I::TEN; // rounding-divide
+            Some(Self::do_pack(sign, scale - 1, man))
         } else {
             None
         }
     }
 
-    pub fn checked_add(self, right: Self) -> Option<Self> {
-        let (a, b, scale) = self.align_scale(right);
-        Self::pack_sum(a + b, scale)
-    }
-
-    pub fn checked_sub(self, right: Self) -> Option<Self> {
-        let (a, b, scale) = self.align_scale(right);
-        if !I::IS_SIGNED && a < b {
-            return None;
-        }
-        Self::pack_sum(a - b, scale)
-    }
-
-    fn align_scale(self, right: Self) -> (I, I, u8) {
-        let (a_man, a_scale) = self.unpack();
-        let (b_man, b_scale) = right.unpack();
-
-        // aligned already
-        if a_scale == b_scale {
-            return (a_man, b_man, a_scale);
-        }
-
-        // big_man/big_scale: the number with bigger scale
-        // small_man/small_scale: the number with smaller scale
-        let (big_man, big_scale, small_man, small_scale) = if a_scale > b_scale {
-            (a_man, a_scale, b_man, b_scale)
-        } else {
-            (b_man, b_scale, a_man, a_scale)
-        };
-
-        let small_avail = small_man.avail_digits();
-        let diff = big_scale - small_scale;
-
-        if diff <= small_avail {
-            (small_man.mul_exp(diff), big_man, big_scale)
-        } else {
-            Self::add_by_dividing_small(big_man, big_scale, small_man, small_avail, diff)
-        }
-    }
-
-    // move the unlikely case into a separate cold function
-    #[cold]
-    fn add_by_dividing_small(
-        big_man: I,
-        big_scale: u8,
-        small_man: I,
-        small_avail: u8,
-        diff: u8,
-    ) -> (I, I, u8) {
-        let small_man = small_man.mul_exp(diff - small_avail);
-        let big_man = big_man.div_exp(small_avail);
-        (small_man, big_man, big_scale - small_avail)
-    }
-
     pub fn checked_mul(self, right: Self) -> Option<Self> {
-        let (a_man, a_scale) = self.unpack();
-        let (b_man, b_scale) = right.unpack();
+        let (a_sign, a_scale, a_man) = self.unpack();
+        let (b_sign, b_scale, b_man) = right.unpack();
 
-        if a_man == I::ZERO || b_man == I::ZERO {
-            return Some(Self::ZERO);
-        }
+        let is_mul_overflow =
+            a_man.leading_zeros() + b_man.leading_zeros() < I::BITS + I::SCALE_BITS + 1;
 
-        let (man, scale) = if !a_man.is_mul_overflow(b_man) {
+        let (man, scale) = if !is_mul_overflow {
             let p = a_man * b_man;
             let sum_scale = a_scale + b_scale;
             if sum_scale <= I::MAX_SCALE {
                 (p, sum_scale)
             } else {
-                let diff = sum_scale - I::MAX_SCALE;
-                (p.div_exp(diff), I::MAX_SCALE)
+                (p.div_exp(sum_scale - I::MAX_SCALE), I::MAX_SCALE)
             }
         } else {
             a_man.mul_with_sum_scale(b_man, a_scale + b_scale)?
         };
 
-        Self::pack(man, scale)
+        Some(Self::do_pack(a_sign ^ b_sign, scale, man))
+    }
+}
+
+// Mark this function as #[cold] to avoid interfering with the main flow of the
+// caller add_or_sub(), thereby enabling better compiler optimizations.
+#[cold]
+fn align_scale<I>(a_man: I, a_scale: u32, b_man: I, b_scale: u32) -> (I, I, u32)
+where
+    I: UnderlyingInt,
+{
+    // big_man/big_scale: the number with bigger scale
+    // small_man/small_scale: the number with smaller scale
+    let (big_man, big_scale, small_man, small_scale) = if a_scale > b_scale {
+        (a_man, a_scale, b_man, b_scale)
+    } else {
+        (b_man, b_scale, a_man, a_scale)
+    };
+
+    let small_avail = small_man.min_avail_digits();
+    let diff = big_scale - small_scale;
+
+    if diff <= small_avail {
+        // rescale small_man to big_scale
+        (small_man.mul_exp(diff), big_man, big_scale)
+    } else {
+        // rescale both small_man and big_man
+        let small_man = small_man.mul_exp(diff - small_avail);
+        let big_man = big_man.div_exp(small_avail);
+        (small_man, big_man, big_scale - small_avail)
     }
 }
 
@@ -185,50 +190,50 @@ mod tests {
 
     #[test]
     fn test_avail_digits() {
-        assert_eq!(0_i128.avail_digits(), 36);
+        assert_eq!(0_u128.min_avail_digits(), 36);
 
-        let max = 2_i128.pow(121) - 1;
-        assert_eq!(max.avail_digits(), 0);
+        let max = 2_u128.pow(121) - 1;
+        assert_eq!(max.min_avail_digits(), 0);
 
-        assert_eq!((max / 10 + 1).avail_digits(), 0);
-        assert_eq!((max / 10).avail_digits(), 1);
-        assert_eq!((max / 10 - 1).avail_digits(), 1);
+        assert_eq!((max / 10 + 1).min_avail_digits(), 0);
+        assert_eq!((max / 10).min_avail_digits(), 0);
+        assert_eq!((max / 10 - 1).min_avail_digits(), 0);
 
-        assert_eq!((max / 1000 + 1).avail_digits(), 2);
-        assert_eq!((max / 1000).avail_digits(), 3);
-        assert_eq!((max / 1000 - 1).avail_digits(), 3);
+        assert_eq!((max / 1000 + 1).min_avail_digits(), 2);
+        assert_eq!((max / 1000).min_avail_digits(), 2);
+        assert_eq!((max / 1000 - 1).min_avail_digits(), 2);
     }
 
     #[test]
     fn simple_add() {
-        let a = Decimal::pack(1000, 10).unwrap();
-        let b = Decimal::pack(1000, 13).unwrap();
-        let r = Decimal::pack(1001000, 13).unwrap();
+        let a = Decimal::pack(0, 10, 1000).unwrap();
+        let b = Decimal::pack(0, 13, 1000).unwrap();
+        let r = Decimal::pack(0, 13, 1001000).unwrap();
         assert_eq!(a.checked_add(b).unwrap(), r);
         assert_eq!(b.checked_add(a).unwrap(), r);
     }
 
     #[test]
     fn test_mul() {
-        let a = Decimal::pack(1000, 10).unwrap();
-        assert_eq!(a.checked_mul(a), Decimal::pack(1000_000, 20));
+        let a = Decimal::pack(0, 10, 1000).unwrap();
+        assert_eq!(a.checked_mul(a), Decimal::pack(0, 20, 1000_000));
 
-        let a = Decimal::pack(1000, 20).unwrap();
-        assert_eq!(a.checked_mul(a), Decimal::pack(100, 36));
+        let a = Decimal::pack(0, 20, 1000).unwrap();
+        assert_eq!(a.checked_mul(a), Decimal::pack(0, 36, 100));
 
-        let a = Decimal::pack(10_i128.pow(20), 10).unwrap();
-        assert_eq!(a.checked_mul(a), Decimal::pack(10_i128.pow(36), 16));
+        let a = Decimal::pack(0, 10, 10_u128.pow(20)).unwrap();
+        assert_eq!(a.checked_mul(a), Decimal::pack(0, 16, 10_u128.pow(36)));
 
         // overflow: (30 + 30) - 36 > 10 + 10
-        let a = Decimal::pack(10_i128.pow(30), 10).unwrap();
+        let a = Decimal::pack(0, 10, 10_u128.pow(30)).unwrap();
         assert_eq!(a.checked_mul(a), None);
 
         // (30 + 30) - 36 > 20 + 20
-        let a = Decimal::pack(10_i128.pow(30), 20).unwrap();
-        assert_eq!(a.checked_mul(a), Decimal::pack(10_i128.pow(36), 16));
+        let a = Decimal::pack(0, 20, 10_u128.pow(30)).unwrap();
+        assert_eq!(a.checked_mul(a), Decimal::pack(0, 16, 10_u128.pow(36)));
 
         // (20 + 20) - 36 < 30 + 30
-        let a = Decimal::pack(10_i128.pow(20), 30).unwrap();
-        assert_eq!(a.checked_mul(a), Decimal::pack(10_i128.pow(16), 36));
+        let a = Decimal::pack(0, 30, 10_u128.pow(20)).unwrap();
+        assert_eq!(a.checked_mul(a), Decimal::pack(0, 36, 10_u128.pow(16)));
     }
 }
